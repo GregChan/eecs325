557111ed458ca009227980a78f0c7a7fb02f13a0
NU CS Intern <nucsintern@shmoop-cs-intern-5.local>
2013-11-01 23:28:13 -0500

    modified chapter3-5.lisp

--- chapter3-5.lisp
+++ chapter3-5.lisp
@@ -1,10 +1,15 @@
 (in-package :cs325-user)
 
-(defun position+ (lst &optional temp)
-  (if (endp lst)) (reverse lst) (position+ (cdr lst) (push temp)))
+; (defun position+ (lst &optional temp)
+;   (if (endp lst) 
+;   	(reverse temp) 
+;   	(position+ (cdr lst) (push (+ (car lst) (length temp)) temp))))
 
 ; (defun position+ (lst)
 ;   (do ((l lst (cdr l))
 ;        (i 0 (1+ i))
 ;        (temp '() (cons (+ (car l) i) temp)))
 ;       ((null l) (reverse temp))))
+
+(defun position+ (list)
+  (mapcar #'(lambda (x) (+ (position x list) x)) list))
