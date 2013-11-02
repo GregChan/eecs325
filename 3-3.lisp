73a4a69140cb0aeaf1c4bc1aef9687ce6f648152
GregChan <gregorywarrenchan@gmail.com>
2013-11-02 01:19:19 -0500

    finished 3-5 and started 3-8

--- 3-3.lisp
+++ 3-3.lisp
@@ -1,7 +1,8 @@
 (in-package :cs325-user)
 
 (defun occurrences (x)
-  (do ((l x (cdr x))
-       (lst '() (cond ((null (assoc (car l) lst)) (push (cons (car l) 1) lst))
-                      (t (incf (cdr (assoc (car l) lst))) lst))))
-      ((null x) lst)))
+  (cond ((null x) nil)
+        (t (do ((l x (cdr l))
+                (lst '() (cond ((null (assoc (car l) lst)) (acons (car l) 1 lst))
+                               (t (incf (cdr (assoc (car l) lst))) lst))))
+               ((null l) (sort lst #'> :key #'cdr))))))
