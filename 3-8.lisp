0fe8e42dc4e2b90742790bd6c1cd203ffe5c1c59
GregChan <gregorywarrenchan@gmail.com>
2013-11-03 01:43:01 -0600

    modified 3-8.lisp with show-list

--- 3-8.lisp
+++ 3-8.lisp
@@ -1,8 +1,22 @@
+(in-package :cs325-user)
+
 (defun show-dots (lst)
   (format t "~A" (generate-dotted-string lst)))
 
-(defun show-lists (lst)
-  (cond ((null lst)) ))
+(defun show-list (lst)
+  (cond ((null lst) (print lst))
+        ((atom lst) (print lst))
+        (t (princ "[")
+           (do ((l lst (cdr l)))
+               ((null l) (princ "]"))
+               (princ (null (cadr l)))
+               (cond ((atom (car l))
+                      (cond ((null (cadr l)) (princ (format nil "~A" (car l))))
+                            (t (princ (format nil "~A " (car l))))))
+                     (t
+                       (cond ((null (cadr l)) (show-list (car l))) 
+                             (t (show-list (car l)) (princ " "))))))))
+  nil)
 
 (defun generate-dotted-string (lst)
   (cond ((null lst) nil)
