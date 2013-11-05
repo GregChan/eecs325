305dd21a861bb91ea105e3eec7de24ccebf72680
GregChan <gregorywarrenchan@gmail.com>
2013-11-04 22:31:07 -0600

    modified has-number-p.lisp

--- has-number-p.lisp
+++ has-number-p.lisp
@@ -2,6 +2,7 @@
 
 (defun has-number-p (lst)
   (cond ((null lst) nil)
+        ((and (listp lst) (some #'numberp lst)) t)
         ((atom lst) (numberp lst))
         (t (or (has-number-p (car lst)) 
                (has-number-p (cdr lst))))))
