ea64ea11a0f5634e92181f7af28c382e4662f104
GregChan <gregorywarrenchan@gmail.com>
2013-11-14 03:22:18 -0600

    modified and submitted key-if

--- key-if.lisp
+++ key-if.lisp
@@ -1,4 +1,18 @@
 (in-package :cs325-user)
 
-(defmacro my-macro (x)
-  ())
+(defmacro key-if (test &rest args)
+  (multiple-value-bind (then else) (parse-then-else args nil nil nil)
+                       (list 'cond
+                             (if (null then) 
+                                 (list test then) 
+                                 (cons test then))
+                             (if (null else)
+                                 (list t else)
+                                 (cons t else)))))
+
+(defun parse-then-else (exp then else flag)
+  (cond ((atom exp) (values (nreverse then) (nreverse else)))
+        ((eql :then (car exp)) (parse-then-else (cdr exp) then else t))
+        ((eql :else (car exp)) (parse-then-else (cdr exp) then else nil))
+        ((null flag) (parse-then-else (cdr exp) then (cons (car exp) else) flag))
+        (t (parse-then-else (cdr exp) (cons (car exp) then) else flag))))
