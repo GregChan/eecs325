e64b2790c214ef5517444c0503d4be0461e8a737
GregChan <gregorywarrenchan@gmail.com>
2013-11-26 02:30:10 -0600

    modified key-if

--- key-if.lisp
+++ key-if.lisp
@@ -1,22 +1,13 @@
 (in-package :cs325-user)
 
 (defmacro key-if (test &rest args)
-  (multiple-value-bind (then else) (parse-then-else args nil nil nil)
-                       (list 'cond
-                             (if (null then) 
-                                 (list test then) 
-                                 (cons test then))
-                             (if (null else)
-                                 (list t else)
-                                 (cons t else)))))
+  '(,cond 
+    ,(cons test (parse :then :else args nil nil)) 
+    ,(cons t (parse :else :then args nil nil))))
 
-(defun parse-between (exp start end lst)
-  (cond ((eql end (car exp)) (nreverse lst))
-        (t (parse-between (cdr exp) end (cons (car exp) lst)))))
-
-(defun parse-then-else (exp then else flag)
-  (cond ((atom exp) (values (nreverse then) (nreverse else)))
-        ((eql :then (car exp)) (parse-then-else (cdr exp) then else t))
-        ((eql :else (car exp)) (parse-then-else (cdr exp) then else nil))
-        ((null flag) (parse-then-else (cdr exp) then (cons (car exp) else) flag))
-        (t (parse-then-else (cdr exp) (cons (car exp) then) else flag))))
+(defun parse (start end exp lst flag)
+  (cond ((eql start (car exp)) (parse start end (cdr exp) lst t))
+        ((or (and flag (eql end (car exp))) (null (car exp))) 
+         (if (null lst) '(nil) (nreverse lst)))
+        (flag (parse start end (cdr exp) (cons (car exp) lst) flag))
+        (t (parse start end (cdr exp) lst nil))))
