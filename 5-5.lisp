b45b0f67fe9ca1b30e3719b9f38042c828b99c2c
GregChan <gregorywarrenchan@gmail.com>
2013-12-02 01:29:11 -0600

    modified 5-5

--- 5-5.lisp
+++ 5-5.lisp
@@ -1,6 +1,5 @@
 (in-package :cs325-user)
 
-;This function receives a critique about the usage of WHEN. It says that it is "not appropriate when the return value is needed." I don't believe the return value of when is important here and thus ignored this critique.
 (defun preceders (x vec)
   (do ((i (position x vec) (position x vec :start (1+ i)))
        (res '() (when (> i 0) (adjoin (aref vec (1- i)) res))))
