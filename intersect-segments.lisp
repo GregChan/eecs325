2b15532a5faf368590b100defd1cf8f4b5601c9b
GregChan <gregorywarrenchan@gmail.com>
2013-12-02 18:31:25 -0600

    submitted intersect-segments modified 5-5

--- intersect-segments.lisp
+++ intersect-segments.lisp
@@ -1,29 +1,27 @@
 (in-package :cs325-user)
 
-;The critique gives me warnings for "intersect-segments", for "parallel-or-disjoint" for "sort-points" that my code is "a little too long." I'm not sure how to fix this. I feel like my definitions are necessary. Please let me know if they are not. 
 (defun intersect-segments (x1 y1 x2 y2 x3 y3 x4 y4)
   (let* ((v1 (list (- x2 x1) (- y2 y1)))
          (v2 (list (- x4 x3) (- y4 y3)))
          (v3 (list (- x1 x3) (- y1 y3)))
          (cross-bottom (cross-product v1 v2)))
     (cond ((= cross-bottom 0) 
-           (parallel-or-disjoint x1 y1 x2 y2 x3 y3 x4 y4))
-          (t (intersecting x1 y1 v1 v2 v3 cross-bottom)))))
+           (parallel-intersection x1 y1 x2 y2 x3 y3 x4 y4))
+          (t (non-parallel-intersection x1 y1 v1 v2 v3 cross-bottom)))))
 
 ;Determines if two line segments are intersecting. If the segments are intersecting then the function returns the values of the intersection point. If not, the function returns nil.
-(defun intersecting (x1 y1 v1 v2 v3 cross-bottom)
+(defun non-parallel-intersection (x1 y1 v1 v2 v3 cross-bottom)
   (let* ((a (/ (cross-product v2 v3) cross-bottom))
          (b (/ (cross-product v1 v3) cross-bottom)))
-    (cond ((and (between-zero-and-one a) (between-zero-and-one b)) (values (+ x1 (* a (car v1))) (+ y1 (* a (cadr v1)))))
-          (t nil))))
+    (and (between-zero-and-one a) (between-zero-and-one b) (values (+ x1 (* a (car v1))) (+ y1 (* a (cadr v1)))))))
 
 ;Determines if a number is between zero and one
 (defun between-zero-and-one (x)
   (>= 1 x 0))
 
 ;Determines if two line segments are parallel or disjoint by using the cross product formula between the two segment vectors. If the two lines overlap, the appropriate overlap is returned. NIL is returned if the lines are disjoint.
-(defun parallel-or-disjoint (x1 y1 x2 y2 x3 y3 x4 y4)
-  (let ((result (multiple-value-call #'coincident (sort-points x1 y1 x2 y2 x3 y3 x4 y4))))
+(defun parallel-intersection (x1 y1 x2 y2 x3 y3 x4 y4)
+  (let ((result (coincident x1 y1 x2 y2 x3 y3 x4 y4)))
     (cond ((null result) nil)
           ((eql (slope x1 y1 x2 y2) 'point)
            (if (eql (slope x3 y3 x4 y4) 'point)
@@ -31,22 +29,15 @@
                (values (car result) (cadr result) (car result) (cadr result))))
           (t (values-list result)))))
 
-;Finds the coincident overlap given two parallel line segments and sorted points. Using the sorted points makes it easier to determine the overlap of the lines. If there is no overlap, returns nil.
+;Finds the coincident overlap given two parallel line segments. If there is no overlap return nil.
 (defun coincident (x1 y1 x2 y2 x3 y3 x4 y4)
-  (cond ((or (and (>= x2 x3) (<= x2 x4))
-             (and (<= x2 x3) (>= x2 x4)))
-         (list x2 y2 x3 y3))
-        (t nil)))
-
-;Sorts the points so that it is easier to determine the overlap of two line segments. Arranges the list such that the point farthest left in the x direction is closest to the front of the list.
-(defun sort-points (x1 y1 x2 y2 x3 y3 x4 y4)
-  (cond ((and (<= x1 x2) (<= x3 x4))
-         (values x1 y1 x2 y2 x3 y3 x4 y4))
-        ((<= x1 x2)
-         (values x1 y1 x2 y2 x4 y4 x3 y3))
-        ((<= x2 x4)
-         (values x2 y2 x1 y1 x3 y3 x4 y4))
-        (t (values x2 y2 x1 y1 x4 y4 x3 y3))))
+  (let ((start (max x1 x2))
+        (end (min x3 x4))
+        (bound (max x3 x4)))
+    (if (or (and (<= start end) (>= start bound))
+            (and (>= start end) (<= start bound)))
+        (list start (max y1 y2) end (min y3 y4))
+        nil)))
 
 ;Takes the cross product of two vectors represented as lists.
 (defun cross-product (v1 v2)
